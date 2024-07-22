ecc02bf281cb0fa4f0caeb264d566c2f0cc6dad9
kuzzneli <52350262+kuzzneli@users.noreply.github.com>
2024-07-22 12:47:57 +0300

    smth new

--- Game/player.py
+++ Game/player.py
@@ -16,10 +16,11 @@ class Player:
 
 
 
-
+    # Варто було б дописати те, як буде додаватись нова частинка тіла змійки при з'їданні їжі
     # def addCube(self):
     #     self.cubes.append(Cube(self))
     def movePlayer(self):
+        # Тут відбувається перевірка на те, куди варто повернути змійці та на яку відстань піти
         if self.direction == 0:
             self.directionPosX = 25
             self.directionPosY = 0
@@ -32,12 +33,17 @@ class Player:
         if self.direction == 3:
             self.directionPosX = 0
             self.directionPosY = -25
+
+        # Змінюємо сторону, в яку ми маємо йти, в залежності від self.direction
         self.cubes[0].fx = self.cubes[0].posX + self.directionPosX
         self.cubes[0].fy = self.cubes[0].posY + self.directionPosY
+        # Рухаємо голову змійки
         self.cubes[0].move(self.cubes[0].fx + self.directionPosX, self.cubes[0].fy + self.directionPosY)
+        # Рухаємо решту змійки
         for i in range(1, len(self.cubes)):
             self.cubes[i].move(self.cubes[i - 1].posX, self.cubes[i - 1].posY)
 
+    # Малюємо тіло змійки
     def drawPlayer(self, screen):
         for i in self.cubes:
             pygame.draw.rect(screen, 'red', i.cube)
